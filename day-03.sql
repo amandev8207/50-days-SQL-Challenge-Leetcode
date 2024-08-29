453217c2eb688b9bfd93b2e3bb85ea5122747771
Aman <72264622+amandev8207@users.noreply.github.com>
2024-08-29 09:34:42 +0000

    add daily challenges

---
+++ day-03.sql
@@ -0,0 +1,24 @@
+#584. Find Customer Referee
+Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Find the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+# Write your MySQL query statement below
+select name from customer where referee_id != 2 or referee_id is null;
