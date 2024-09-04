3672e71f11712b82aaf743391c4fb4dfd62fa517
Aman <72264622+amandev8207@users.noreply.github.com>
2024-09-04 06:14:36 +0000

    add dialy project

---
+++ day-09.sql
@@ -0,0 +1,25 @@
+197. Rising Temperature
+
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+# Write your MySQL query statement below
+select w2.id from weather w1, weather w2
+where datediff(w2.recordDate, w1.recordDate) = 1 and w2.temperature > w1.temperature;
