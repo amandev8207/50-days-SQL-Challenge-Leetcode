d14de633e9639ee35f1859ea924376644907f4c4
Aman <72264622+amandev8207@users.noreply.github.com>
2024-09-08 13:06:15 +0000

    add daily project

---
+++ day-13.sql
@@ -0,0 +1,56 @@
+#1280. Students and Examinations
+
+Table: Students
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| student_name  | varchar |
++---------------+---------+
+student_id is the primary key (column with unique values) for this table.
+Each row of this table contains the ID and the name of one student in the school.
+ 
+
+Table: Subjects
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| subject_name | varchar |
++--------------+---------+
+subject_name is the primary key (column with unique values) for this table.
+Each row of this table contains the name of one subject in the school.
+ 
+
+Table: Examinations
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| student_id   | int     |
+| subject_name | varchar |
++--------------+---------+
+There is no primary key (column with unique values) for this table. It may contain duplicates.
+Each student from the Students table takes every course from the Subjects table.
+Each row of this table indicates that a student with ID student_id attended the exam of subject_name.
+ 
+
+Write a solution to find the number of times each student attended each exam.
+
+Return the result table ordered by student_id and subject_name.
+
+The result format is in the following example.
+
+# Write your MySQL query statement below
+
+SELECT 
+stu.student_id, stu.student_name, sub.subject_name, COUNT(exam.subject_name) AS attended_exams
+FROM STUDENTS AS stu
+CROSS JOIN SUBJECTS AS sub
+LEFT JOIN EXAMINATIONS AS exam
+ON exam.student_id = stu.student_id AND sub.subject_name = exam.subject_name
+GROUP BY
+stu.student_id, stu.student_name, sub.subject_name
+ORDER BY
+stu.student_id, sub.subject_name;
