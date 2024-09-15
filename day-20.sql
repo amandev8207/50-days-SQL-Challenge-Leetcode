211afae9b721a2022706aba5f8cdbe2f51eadba9
Aman <72264622+amandev8207@users.noreply.github.com>
2024-09-15 14:03:59 +0000

    add daily challenges

---
+++ day-20.sql
@@ -0,0 +1,38 @@
+#1633. Percentage of Users Attended a Contest
+
+Table: Users
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| user_id     | int     |
+| user_name   | varchar |
++-------------+---------+
+user_id is the primary key (column with unique values) for this table.
+Each row of this table contains the name and the id of a user.
+ 
+
+Table: Register
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| contest_id  | int     |
+| user_id     | int     |
++-------------+---------+
+(contest_id, user_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id of a user and the contest they registered into.
+ 
+
+Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+The result format is in the following example.
+
+/* Write your T-SQL query statement below */
+select contest_id, 
+round(count(user_id) * 100.0 / (select count(user_id) from Users), 2) as percentage
+from Register
+group by contest_id
+order by percentage desc, contest_id;
