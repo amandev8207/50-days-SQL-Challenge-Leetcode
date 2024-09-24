3caa6f8e7cffc465df5a4580739bd0ed77397e8f
Aman <72264622+amandev8207@users.noreply.github.com>
2024-09-24 15:34:07 +0000

    add daily challenges

---
+++ day-29.sql
@@ -0,0 +1,25 @@
+#1729. Find Followers Count
+
+Table: Followers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| user_id     | int  |
+| follower_id | int  |
++-------------+------+
+(user_id, follower_id) is the primary key (combination of columns with unique values) for this table.
+This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+ 
+
+Write a solution that will, for each user, return the number of followers.
+
+Return the result table ordered by user_id in ascending order.
+
+The result format is in the following example.
+
+/* Write your PL/SQL query statement below */
+select user_id , count(follower_id) as followers_count
+from followers
+group by user_id
+order by user_id;
