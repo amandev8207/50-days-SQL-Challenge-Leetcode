9aacf5a85d8f0f4d6d15e0def5fddfebd9c9efe3
Aman <72264622+amandev8207@users.noreply.github.com>
2024-09-25 17:25:05 +0000

    add daily projects

---
+++ day-30.sql
@@ -0,0 +1,28 @@
+#619. Biggest Single Number
+
+Table: MyNumbers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+Each row of this table contains an integer.
+ 
+
+A single number is a number that appeared only once in the MyNumbers table.
+
+Find the largest single number. If there is no single number, report null.
+
+The result format is in the following example.
+
+
+# Write your MySQL query statement below
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS unique_numbers;
