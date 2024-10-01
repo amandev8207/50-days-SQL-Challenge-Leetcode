18ad0910f36bce89861c84a8957da47a763a14ac
Aman <72264622+amandev8207@users.noreply.github.com>
2024-10-01 15:59:39 +0000

    add daily challenges

---
+++ day-35.sql
@@ -0,0 +1,28 @@
+#180. Consecutive Numbers
+
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column starting from 1.
+ 
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any orde
+
+# Write your MySQL query statement below
+with cte as (
+    select num,
+    lead(num,1) over() num1,
+    lead(num,2) over() num2
+    from logs
+
+)
+
+select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
