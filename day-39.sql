52154826c0e7a374af16aa2d05df81027148aa3d
Aman <72264622+amandev8207@users.noreply.github.com>
2024-10-05 17:18:22 +0000

    add daily challenges

---
+++ day-39.sql
@@ -0,0 +1,25 @@
+#1978. Employees Whose Manager Left the Company
+
+Table: Employees
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| employee_id | int      |
+| name        | varchar  |
+| manager_id  | int      |
+| salary      | int      |
++-------------+----------+
+In SQL, employee_id is the primary key for this table.
+This table contains information about the employees, their salary, and the ID of their manager. Some employees do not have a manager (manager_id is null). 
+ 
+
+Find the IDs of the employees whose salary is strictly less than $30000 and whose manager left the company. When a manager leaves the company, their information is deleted from the Employees table, but the reports still have their manager_id set to the manager that left.
+
+Return the result table ordered by employee_id.
+
+# Write your MySQL query statement below
+SELECT employee_id
+FROM Employees
+WHERE salary<30000 AND manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id
