9286c6c869640fd06e8c746e6f8d71f75a66a9e8
Aman <72264622+amandev8207@users.noreply.github.com>
2024-10-06 17:47:02 +0000

    add daily challenges

---
+++ day-40.sql
@@ -0,0 +1,28 @@
+#626. Exchange Seats
+
+Table: Seat
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key (unique value) column for this table.
+Each row of this table indicates the name and the ID of a student.
+The ID sequence always starts from 1 and increments continuously.
+ 
+
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+Return the result table ordered by id in ascending order.
+
+
+# Write your MySQL query statement below
+SELECT 
+    id,
+    CASE
+        WHEN id % 2 = 0 THEN LAG(student) OVER(ORDER BY id)
+        ELSE COALESCE(LEAD(student) OVER(ORDER BY id), student)
+    END AS student
+FROM Seat
