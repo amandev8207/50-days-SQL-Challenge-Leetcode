d3e2da972a8feaec5071a73d3eb6c22b0bb885e5
Aman <72264622+amandev8207@users.noreply.github.com>
2024-10-15 18:29:58 +0000

    add daily challenges

---
+++ day-49.sql
@@ -0,0 +1,27 @@
+176. Second Highest Salary
+Solved
+Medium
+Topics
+Companies
+SQL Schema
+Pandas Schema
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas)
+
+
+select
+(select distinct Salary 
+from Employee order by salary desc 
+limit 1 offset 1) 
+as SecondHighestSalary;
